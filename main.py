6a092a54ecdf2eff6281bca061874e3dbee126e0
William Schmitt <William Schmitt>
2016-06-05 14:55:25 -0400

    Removing site from controller. Forking into a different repository at brewing/joulia-webserver

---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+Created on Apr 5, 2016
+
+@author: William
+'''
+
+from controls.brewery import brewing
+
+# from tornado import ioloop
+from tornado import gen
+import logging
+
+@gen.coroutine
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    brewery = brewing.brewery()
+    #ioloop.IOLoop.current().run_sync(brewery)
+    
+if __name__ == "__main__":
+    main()
